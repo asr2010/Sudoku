291ec0e007546739c65c439c4ee0ac95d1d70a63
Arpit Raj <arpitshekharraj@gmail.com>
2022-02-17 23:38:05 -0500

    Working Sudoku Logic

--- working.py
+++ working.py
@@ -10,8 +10,28 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
-def valid(bo, num, pos):
+def solve(bo):
 
+    find = find_empty(bo)
+    if not find:
+         return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+    return False
+
+
+
+
+def valid(bo, num, pos):
     #check row
     for i in range(len(bo[0])):
         if bo[pos[0]] [i] == num and pos[1] != i:
@@ -52,4 +72,12 @@ def find_empty(bo):
         for j in range(len(bo[0])):
             if bo[i][j] == 0:
                 return (i, j) #row ,col
+    return False
+
+print_board(board)
+solve(board)
+print("")
+print("====================================")
+print("")
+print_board(board)
 
